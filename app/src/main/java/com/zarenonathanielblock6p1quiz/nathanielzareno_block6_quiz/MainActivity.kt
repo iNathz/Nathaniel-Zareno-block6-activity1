09050a0f5d4b47b3a1bc3568a7c5877e9500950d
Nathaniel Zareno <nathanielzareno794@gmail.com>
2023-12-08 15:05:47 +0800

    Fourth Commit - MainActivity KT - Codes & updates

--- app/src/main/java/com/zarenonathanielblock6p1quiz/nathanielzareno_block6_quiz/MainActivity.kt
+++ app/src/main/java/com/zarenonathanielblock6p1quiz/nathanielzareno_block6_quiz/MainActivity.kt
@@ -2,10 +2,48 @@
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+ class MainActivity : AppCompatActivity() , View.OnClickListener{
+
+    lateinit var btnAdd : Button
+    lateinit var btnSub : Button
+    lateinit var btnMul : Button
+    lateinit var btnDiv : Button
+    lateinit var etF : EditText
+    lateinit var etS : EditText
+    lateinit var resultTV : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btnAdd = findViewById(R.id.button_add)
+        btnSub = findViewById(R.id.button_sub)
+        btnMul = findViewById(R.id.button_mul)
+        btnDiv = findViewById(R.id.button_div)
     }
-}
+
+     override fun onClick(v: View?) {
+         var f = etF.text.toString().toDouble()
+         var s = etS.text.toString().toDouble()
+         var result = 0.0
+         when(v?.id){
+             R.id.button_add -> {
+                 result = f+s
+             }
+             R.id.button_sub -> {
+                 result = f-s
+             }
+             R.id.button_mul -> {
+                 result = f*s
+             }
+             R.id.button_div -> {
+                 result = f/s
+             }
+         }
+         resultTV.text = "Result is $result"
+     }
+ }
